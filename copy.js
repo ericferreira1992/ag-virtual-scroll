1c985ef396290bcfeb31520f3e7166475457af05
Eric Ferreira <ericferreira1992@gmail.com>
2023-12-05 09:21:59 -0300

    chore: rename project and budget adjusted

--- copy.js
+++ copy.js
@@ -1,5 +1,3 @@
 let cpx = require('cpx');
-
-cpx.copy('src/ag-virtual-scroll/*.scss', 'dist');
-cpx.copy('LICENSE', 'dist');
-cpx.copy('README.md', 'dist');
+cpx.copy('LICENSE', 'dist/library');
+cpx.copy('README.md', 'dist/library');
